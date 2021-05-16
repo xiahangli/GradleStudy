3c80e21ecb3380391936068222059ef9c1c25107
xiahangli <2427417167@qq.com>
2021-05-17 01:32:40 +0800

    学习testApi的使用

---
+++ httpwrapper/src/main/java/com/example/httpwrapper/Test.java
@@ -0,0 +1,12 @@
+package com.example.httpwrapper;
+
+/**
+ * @author Henry
+ * @Date 2021/5/17  1:31 AM
+ * @Email 2427417167@qq.com
+ */
+public class Test {
+    void test(){
+//        AbsListViewBindingAdapter a = new AbsListViewBindingAdapter();
+    }
+}
--- httpwrapper/src/test/java/com/example/httpwrapper/ExampleUnitTest.java
+++ httpwrapper/src/test/java/com/example/httpwrapper/ExampleUnitTest.java
@@ -1,5 +1,7 @@
 package com.example.httpwrapper;
 
+import androidx.databinding.adapters.AbsListViewBindingAdapter;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -12,6 +14,7 @@ import static org.junit.Assert.*;
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
+        AbsListViewBindingAdapter a = new AbsListViewBindingAdapter();
         assertEquals(4, 2 + 2);
     }
 }
